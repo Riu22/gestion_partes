45143e97ae7015fc20839a4d1d3492d222bd036a
riu22 <riutordjaume@gmail.com>
2026-03-20 14:08:36 +0100

    first commit

---
+++ src/main/java/com/example/gestion_partes/GestionPartesApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestion_partes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPartesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPartesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestion_partes/GestionPartesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestion_partes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPartesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
